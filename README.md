82f0596f4aab56c05ed1f2bbe5709162c7c8a4d1
thglaser <thglaser@amazon.com>
2025-12-03 11:17:49 +0000

    day 3

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
-![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
@@ -70,9 +70,9 @@ Both parts: 449.058ms
 
 ```
 Day 03
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 55.954ms
+Time part 2: 223.623ms
+Both parts: 279.577ms
 ```
 
 ```
@@ -139,8 +139,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 4/50
-Total time: 460.652ms
+Total stars: 6/50
+Total time: 740.229ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day03/index.ts
@@ -0,0 +1,63 @@
+import run from "aocrunner";
+import { memoise } from "../utils/index.js";
+
+const parse = (input: string) => input.split("\n");
+
+const maxJoltage = memoise((bank: string, batteries: number): string => {
+  if (batteries === 0 || bank.length === 0) {
+    return "";
+  }
+
+  const head = bank[0];
+  const rest = bank.substring(1);
+
+  const withHead = head + maxJoltage(rest, batteries - 1);
+  const withoutHead = maxJoltage(rest, batteries);
+
+  return +withHead > +withoutHead ? withHead : withoutHead;
+})
+
+function sum(list: number[]) {
+  return list.reduce((prev, curr) => prev + curr, 0)
+}
+
+const part1 = (rawInput: string) => {
+  const input = parse(rawInput);
+
+  return sum(input.map(bank => +maxJoltage(bank, 2)))
+};
+
+const part2 = (rawInput: string) => {
+  const input = parse(rawInput);
+
+  return sum(input.map(bank => +maxJoltage(bank, 12)))
+};
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `987654321111111
+811111111111119
+234234234234278
+818181911112111`,
+        expected: 357,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `987654321111111
+811111111111119
+234234234234278
+818181911112111`,
+        expected: 3121910778619,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
